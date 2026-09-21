5f8d288289904203e18797f1b0912f6ab852712c
Kush Prakash <63534055+kushprakash@users.noreply.github.com>
2026-09-21 14:56:49 +0530

    updates

--- app/Http/Controllers/Banking/MerchantController.php
+++ app/Http/Controllers/Banking/MerchantController.php
@@ -1271,7 +1271,7 @@ class MerchantController extends Controller
                 return response()->json([
                     'status'  => 0,
                     'pending_status'  => 3,
-                    'message' => 'Email not verifieds- '.$existingUser->email_verified_at,
+                    'message' => 'Email not verified',
                 ], 200);
             }
 
@@ -1306,6 +1306,7 @@ class MerchantController extends Controller
                 "aeps_draft_id" => $existingUser->bid,
                 "outletId"      => $existingUser->bmid,
                 "pan_no"        => $existingUser->pan_no,
+                "kyc_done"  => true
             ];
 
             $ch = curl_init($url);
